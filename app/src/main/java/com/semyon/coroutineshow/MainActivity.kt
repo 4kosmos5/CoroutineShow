59b649520b277211eb1df5923d92b2d3f033d468
4kosmos5 <s.altushkin@wellsoft.pro>
2024-03-26 19:13:40 +0500

    finish

--- app/src/main/java/com/semyon/coroutineshow/MainActivity.kt
+++ app/src/main/java/com/semyon/coroutineshow/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.semyon.coroutineshow.databinding.ActivityMainBinding
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -19,7 +21,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
-            lifecycleScope.launch { loadData() }
+            binding.progress.isVisible = true
+            binding.buttonLoad.isEnabled = false
+            val deferredCity: Deferred<String> = lifecycleScope.async {
+                val city = loadCity()
+                binding.tvLocation.text = city
+                city
+            }
+            val deferredTemp: Deferred<Int> = lifecycleScope.async {
+                val temp = loadTemperature()
+                binding.tvTemperature.text = temp.toString()
+                temp
+            }
+            lifecycleScope.launch {
+                val city = deferredCity.await()
+                val temp = deferredTemp.await()
+                Toast.makeText(this@MainActivity, "City: $city Temp: $temp", Toast.LENGTH_SHORT).show()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
         }
     }
 
@@ -28,23 +48,18 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.isEnabled = false
         val city = loadCity()
         binding.tvLocation.text = city
-        val temp = loadTemperature(city)
+        val temp = loadTemperature()
         binding.tvTemperature.text = temp.toString()
         binding.progress.isVisible = false
         binding.buttonLoad.isEnabled = true
     }
 
-    private suspend fun loadCity() : String {
-        delay(5000)
+    private suspend fun loadCity() :  String {
+        delay(3000)
         return "Ekaterinburg"
     }
 
-    private suspend fun loadTemperature(city: String) : Int {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
+    private suspend fun loadTemperature() : Int {
         delay(5000)
         return 25
     }
